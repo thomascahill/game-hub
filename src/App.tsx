98c8344807445d15b40c6674e70a57a7f3bb42ca
thomascahill <cahill89@gmail.com>
2023-09-21 20:53:34 -0500

    Filter games by platform

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,14 @@ import GenreList from "./components/GenreList.tsx";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres.ts";
 import PlatformSelector from "./components/PlatformSelector.tsx";
+import { Platform } from "./hooks/useGames.ts";
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(
+    null
+  );
+
   return (
     <>
       <Grid
@@ -32,8 +37,14 @@ function App() {
           </GridItem>
         </Show>
         <GridItem area="main" paddingX={5}>
-          <PlatformSelector />
-          <GameGrid selectedGenre={selectedGenre} />
+          <PlatformSelector
+            selectedPlatform={selectedPlatform}
+            onSelectPlatform={(platform) => setSelectedPlatform(platform)}
+          />
+          <GameGrid
+            selectedPlatform={selectedPlatform}
+            selectedGenre={selectedGenre}
+          />
         </GridItem>
       </Grid>
     </>
--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,8 +1,17 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDoubleDown } from "react-icons/bs";
 import usePlatforms from "../hooks/usePlatforms";
+import { Platform } from "../hooks/useGames";
 
-export default function PlatformSelector() {
+interface Props {
+  onSelectPlatform: (platform: Platform) => void;
+  selectedPlatform: Platform | null;
+}
+
+export default function PlatformSelector({
+  onSelectPlatform,
+  selectedPlatform,
+}: Props) {
   const { data, error } = usePlatforms();
 
   if (error) {
@@ -10,14 +19,18 @@ export default function PlatformSelector() {
   }
 
   let platformItem = data.map((platform) => {
-    return <MenuItem key={platform.id}>{platform.name}</MenuItem>;
+    return (
+      <MenuItem onClick={() => onSelectPlatform(platform)} key={platform.id}>
+        {platform.name}
+      </MenuItem>
+    );
   });
 
   return (
     <>
       <Menu>
         <MenuButton as={Button} rightIcon={<BsChevronDoubleDown />}>
-          Platforms
+          {selectedPlatform?.name || "Platforms"}
         </MenuButton>
         <MenuList>{platformItem}</MenuList>
       </Menu>
