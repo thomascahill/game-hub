875d178a42e400c116f56e356bd751c59455166b
thomascahill <cahill89@gmail.com>
2023-09-22 09:40:47 -0500

    Build search input

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -11,7 +11,7 @@ export default function ColorModeSwitch() {
           isChecked={colorMode === "dark"}
           onChange={toggleColorMode}
         />
-        <Text>Dark Mode</Text>
+        <Text whiteSpace="nowrap">Dark Mode</Text>
       </HStack>
     </>
   );
