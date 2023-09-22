248cb0d01c2c12b4c9d5e386b93f09088387beed
thomascahill <cahill89@gmail.com>
2023-09-22 14:19:46 -0500

    Rainbow effect header title

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -1,6 +1,7 @@
 import { Heading } from "@chakra-ui/react";
 
 import { GameQuery } from "../App";
+import { useState, useEffect } from "react";
 
 interface Props {
   gameQuery: GameQuery;
@@ -10,9 +11,27 @@ export default function GameHeading({ gameQuery }: Props) {
   const heading = `${gameQuery.platform?.name || ""} ${
     gameQuery.genre?.name || ""
   } Games`;
+
+  const colors = ["#FF5733", "#33FF57", "#5733FF", "#FFFF33"];
+  const [currentColorIndex, setCurrentColorIndex] = useState(0);
+
+  const changeTextColor = () => {
+    setCurrentColorIndex((currentColorIndex + 1) % colors.length);
+  };
+
+  useEffect(() => {
+    const timer = setInterval(changeTextColor, 2000);
+    return () => clearInterval(timer);
+  }, [currentColorIndex]);
+
   return (
     <>
-      <Heading as="h1" fontSize="50px">
+      <Heading
+        id="changingText"
+        as="h1"
+        fontSize="50px"
+        color={colors[currentColorIndex]}
+      >
         {heading}
       </Heading>
     </>
